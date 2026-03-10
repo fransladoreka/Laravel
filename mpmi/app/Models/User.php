9c96a3fa653f1d9595c5242a37cc5ac1e662131a
Fransiskus Lado <franslado@gmail.com>
2026-03-10 10:00:21 +0700

    ubah ulid pada file create migrations

--- mpmi/app/Models/User.php
+++ mpmi/app/Models/User.php
@@ -7,12 +7,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
     use HasRoles;
+    use HasUlids;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
 
     /**
      * The attributes that are mass assignable.
